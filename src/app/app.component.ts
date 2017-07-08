cff0241d2ad836653f45518cb9fdd27b49003719
RodolfoLeiva <Rodolfoleivacl@gmail.com>
2017-07-08 19:00:45 -0300

    segunda etapa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { MdSidenav } from '@angular/material';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.less'],
   providers: [SidenavService]
 })
 
